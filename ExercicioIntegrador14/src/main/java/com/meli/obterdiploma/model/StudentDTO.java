d6f850e9f22a11ad6d4dd3dc6459352b3f813b21
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-10-10 17:51:51 -0300

    exercício para estudo de validacoes

---
+++ ExercicioIntegrador14/src/main/java/com/meli/obterdiploma/model/StudentDTO.java
@@ -0,0 +1,24 @@
+package com.meli.obterdiploma.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+public class StudentDTO {
+    @NotEmpty(message = "O nome do aluno não pode ficar vazio")
+    @Pattern(regexp = "^[A-Z][a-záéíóúàèìòùâêîôûãõ]+", message = "A inicial do nome deve ser maiuscula")
+    @Size(max = 50, message = "o comprimento do nome não pode exceder 50 caracteres")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "A lista não pode estar vazia")
+    List<@Valid SubjectDTO> subjects;
+}
---
+++ ExercicioIntegrador14/src/main/java/com/meli/obterdiploma/model/SubjectDTO.java
@@ -0,0 +1,20 @@
+package com.meli.obterdiploma.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+@Data
+public class SubjectDTO {
+    @NotEmpty(message = "O nome do assunto não pode ficar vazio")
+    @Pattern(regexp = "^[A-Z][a-záéíóúàèìòùâêîôûãõ]+", message = "A inicial do nome da matéria deve ser maiuscula")
+    @Size(max = 30, message = "O comprimento do nome não pode exceder 30 caracteres")
+    String name;
+
+    @DecimalMin(value = "0.0", message = "A nota mínima é 0.0")
+    @DecimalMax(value = "10.0", message = "A nota máxima é 10.0")
+    Double score;
+}
